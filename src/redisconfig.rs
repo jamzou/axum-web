1f4189c15c0e3dfeb04c16d99d41fcbd1301d503
邹悦鉴 <zyj0815@qq.com>
2025-11-28 22:34:21 +0800

    redis正确配置

--- src/redisconfig.rs
+++ src/redisconfig.rs
@@ -1,6 +1,8 @@
 use redis::AsyncTypedCommands;
 use redis::Client;
+use redis::aio::MultiplexedConnection;
 use tracing::error;
+use tracing::info;
 use std::env;
 
 use crate::context::jamerr::AppErr;
@@ -8,9 +10,13 @@ use crate::context::jamerr::AppErr;
 pub async fn init_redis() -> RedisTemplate {
     // 从环境变量获取 Redis URL
     let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
-    println!("Redis URL: {}", redis_url); // 这会打印出解码后的 URL
+    info!("Redis URL: {}", redis_url); // 这会打印出解码后的 URL
 
-    let client = redis::Client::open(redis_url).expect("初始化redis连接出错");
+    let client = redis::Client::open(redis_url)
+        .expect("初始化redis连接出错");
+    client.get_multiplexed_async_connection()
+        .await
+        .expect("初始化redis连接出错");
     return RedisTemplate {
         client,
     };
@@ -21,14 +27,44 @@ pub struct RedisTemplate {
 }
 
 impl RedisTemplate {
+    // 提取公共方法获取 Redis 连接
+    async fn get_connection(&self) -> Result<MultiplexedConnection, AppErr> {
+        self.client.get_multiplexed_tokio_connection()
+            .await
+            .map_err(|e| {
+                error!("redis连接出错:{}", e);
+                AppErr::DbError(e.to_string())
+            })
+    }
+    
     pub async fn redis_set(&self, key: &str, value: &str) -> Result<(), AppErr> {
-        let mut con = self.client.get_multiplexed_tokio_connection()
-        .await.map_err(|e| {
-            error!("redis连接出错:{}", e);
-            AppErr::DbError(e.to_string())
-        })?;
+        let mut con = self.get_connection().await?;
         let _ = con.set(key, value).await.map_err(|e| AppErr::DbError(e.to_string()))?;
         Ok(())
+    }
+
+    pub async fn redis_get(&self, key: &str) -> Result<Option<String>, AppErr> {
+        let mut con = self.get_connection().await?;
+        let value = con.get(key).await.map_err(|e| AppErr::DbError(e.to_string()));
+        value
+    }
+
+    pub async fn redis_exists(&self, key: &str) -> Result<bool, AppErr> {
+        let mut con = self.get_connection().await?;
+        let exists = con.exists(key).await.map_err(|e| AppErr::DbError(e.to_string()));
+        return exists;
+    }
+
+    pub async fn redis_del(&self, key: &str) -> Result<usize, AppErr> {
+        let mut con = self.get_connection().await?;
+        let del = con.del(key).await.map_err(|e| AppErr::DbError(e.to_string()));
+        return del;
+    }
+
+    pub async fn redis_unlink(&self, key: &str) -> Result<usize, AppErr> {
+        let mut con = self.get_connection().await?;
+        let unlink = con.unlink(key).await.map_err(|e| AppErr::DbError(e.to_string()));
+        return unlink;
     }
 }
 
